adf8d611318a27e29bc7b12fe169e6b606fa094b
hallchr <hallchr14@gmail.com>
2021-06-06 18:14:18 +0100

    include data to change to long format.

--- src/data_wrangling.R
+++ src/data_wrangling.R
@@ -11,3 +11,13 @@ castaways_select <- castaways_tb %>%
   arrange(desc(age))
 
 
+## you want included in the long format
+## it will leave the other variables as is
+castaways_long <- castaways_select %>%
+  pivot_longer(c(age, age_group, state, personality_type, result), 
+               names_to = "variable", 
+               values_to = "value")
+
+
+map_dbl(castaways_select, mean)
+
